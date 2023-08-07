93a25f5d1a53bf121aa825cdcb3b751e1ea71914
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-08-07 23:15:56 +0900

    GIt-setting shell

---
+++ git-setting.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd ~ 
+
+git config --global core.precomposeunicode true
+git config --global core.quotepath false
+
+showGitConfig=""
+
+echo "Show Git config? (Y/N)" 
+read checkAdd
+
+if [ "$showGitConfig" = "Y" ]; then
+    echo "Git Setting Success!"
+    exit 0
+fi    
