4e7615b260cb08799bf8c433dcb3a84889f80636
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-08-09 18:24:14 +0900

    Basic-Shell Logger

--- basic-shell.sh
+++ basic-shell.sh
@@ -4,26 +4,59 @@
 
 # 입력값 -> Y/N 여부 function
 # # ? 조건문을 한줄로 줄인다. 
-inputYN() {
-    local message="${1:-}"
-    echo "$message (Y/N)? "
-    while read answer; do
-        if [[ $answer = [YyNn] ]]; then
-        [[ $answer = [Yy] ]] && rtn=0
-        [[ $answer = [Nn] ]] && rtn=1
-        break
-        fi
-    done
+# inputYN() {
+#     local message="${1:-}"
+#     echo "$message (Y/N)? "
+#     while read answer; do
+#         if [[ $answer = [YyNn] ]]; then
+#         [[ $answer = [Yy] ]] && rtn=0
+#         [[ $answer = [Nn] ]] && rtn=1
+#         break
+#         fi
+#     done
 
-    return $rtn
+#     return $rtn
+# }
+
+# echo "SHELL TEST"
+
+# if inputYN "YN TEST"; then
+#     echo "Y"
+# else 
+#     echo "N"
+# fi
+
+# exit 0
+
+# 출력 형식 정의
+logger() {
+  local variant="${1:-info}"
+  local message="${2:-}"
+
+  if [ -z "$message" ]; then
+    return
+  fi
+
+  case "$variant" in
+    info)
+      printf "\033[34m%-8s\033[0m %s\n" "info" "$message"
+      ;;
+    success)
+      printf "\033[32m%-8s\033[0m %s\n" "success" "$message"
+      ;;
+    warning)
+      printf "\033[33m%-8s\033[0m %s\n" "warning" "$message"
+      ;;
+    error)
+      printf "\033[31m%-8s\033[0m %s\n" "error" "$message"
+      ;;
+  esac
 }
 
-echo "SHELL TEST"
+logger "info" "THIS IS INFO"
 
-if inputYN "YN TEST"; then
-    echo "Y"
-else 
-    echo "N"
-fi
+logger "success" "THIS IS SUCCESS"
+logger "warning" "THIS IS WARNING"
+logger "error" "THIS IS error"
 
-exit 0
+logger "" "TEST"
